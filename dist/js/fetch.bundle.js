f02ed87e20e618ea788a7855b6b1207932c58d2b
Tasio Mendez <tm.tasio@gmail.com>
2020-08-04 22:22:20 +0200

    JS files updated

--- dist/js/fetch.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{205:function(n,w,o){"use strict";o.r(w);o(189),o(190)}}]);
--- dist/js/shadydom.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{187:function(n,w,o){"use strict";o.r(w);o(188),o(192)}}]);
